914a99b1569d46f7e1bc3ae7b0928a5b8ba7b7a3
Sergei Kusakin <kusakinsv@mail.ru>
2026-07-20 12:33:17 +0500

    Модификация vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,36 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-const getEnv = (key: string, fallback: string) => {
-  return process.env[key] || fallback
-}
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
 
-export default defineConfig({
-
-  plugins: [react()],
-  envDir: "./", // явно указываем папку с .env файлами
-  define: {
-    'import.meta.env.PROXY_URL': process.env.VITE_CORE_URL,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src/"),
+    plugins: [react()],
+    // envDir: "./", // явно указываем папку с .env файлами
+    define: {
+      "import.meta.env.PROXY_URL": process.env.VITE_CORE_URL,
     },
-  },
-  server: {
-    host: "0.0.0.0",
-    proxy: {
-      "/search-service/api/v1": {
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/search-service/, ""),
-        target: getEnv("VITE_SEARCHER_URL", "http://localhost:8102"),
-      },
-      "/stream-service/api/v1/": {
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/stream-service/, ""),
-        target: getEnv("VITE_CORE_URL", "http://localhost:8101"),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src/"),
       },
     },
-  },
+    server: {
+      port: 5100,
+      host: "0.0.0.0",
+      proxy: {
+        "/search-service/api/v1": {
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/search-service/, ""),
+          target: env.VITE_SERCHER_URL || "http://localhost:8102",
+        },
+        "/stream-service/api/v1": {
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/stream-service/, ""),
+          target: env.VITE_CORE_URL || "http://localhost:8101",
+        },
+      },
+    },
+  };
 });
